2e7b1f1785eb3cafeb3e7f7cb21999f314e9f39f
MisterX25 <Xavier.Carrel@cpnv.ch>
2021-04-03 17:41:06 +0200

    Login with API OK

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 
 class UserController extends Controller
@@ -12,15 +13,23 @@ class UserController extends Controller
      * get an API token, providing a valid password hash
      */
     public function getToken(Request $request) {
-        $user = User::where('initials',$request->input('initials'))->first();
-        if (password_verify($request->input('password'), $user->password)) {
-            if (!$user->token) {
-                $user->token = Str::random(60);
-                $user->save();
+        if (env('APP_DEBUG')) Log::info(__METHOD__);
+        // don't know why, but React sends arrays in inputs
+        extract($request->input('initials')); // $initials
+        extract($request->input('password')); // $password
+        $user = User::where('initials',$initials)->first();
+        if ($user) {
+            if (password_verify($password, $user->password)) {
+                if (!$user->token) { // generate a new token
+                    $user->token = Str::random(60);
+                    $user->save();
+                }
+                return json_encode(['token' => $user->token]); // Login success
+            } else {
+                return response('Invalid credentials',401); // bad password
             }
-            return json_encode(['token' => $user->token]);
         } else {
-            return response('Invalid credentials',401);
+            return response('Invalid credentials',401); // bad usernamre
         }
     }
 }
