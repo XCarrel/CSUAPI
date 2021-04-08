0085e9555d28ea6c9695c61eaf1fc8ca8174d9d9
MisterX25 <Xavier.Carrel@cpnv.ch>
2021-04-08 13:03:39 +0200

    Format drugreports same as shift reports

--- app/Http/Resources/DrugsheetResource.php
+++ app/Http/Resources/DrugsheetResource.php
@@ -14,6 +14,10 @@ class DrugsheetResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'week' => $this->week,
+            'base' => $this->base->name
+        ];
     }
 }
